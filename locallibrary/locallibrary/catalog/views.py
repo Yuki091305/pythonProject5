041c279dcbdbb65b7e2e143e2fb63ba3261e60db
Yuki091305 <yukifieri@gmail.com>
2025-11-26 13:56:27 +0700

    Initial commit

--- locallibrary/locallibrary/catalog/views.py
+++ locallibrary/locallibrary/catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 # Create your views here.
 from .models import Book, Author, BookInstance, Genre
 
@@ -21,3 +22,9 @@ def index(request):
         context={'num_books': num_books, 'num_instances': num_instances,
                  'num_instances_available': num_instances_available, 'num_authors': num_authors},
     )
+
+class BookListView(generic.ListView):
+    model = Book
+    context_object_name = 'my_book_list'   # ваше собственное имя переменной контекста в шаблоне
+    queryset = Book.objects.filter(title__icontains='war')[:5] # Получение 5 книг, содержащих слово 'war' в заголовке
+    template_name = 'books/my_arbitrary_template_name_list.html'  # Определение имени вашего шаблона и его расположения
